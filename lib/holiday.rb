8196bd8298ee020e9e8bad000af612f7b86349c5
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-12-04 13:52:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,55 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+ holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash.each do |season,data|
+    data.each do |key,hols|
+      hols << supply
+    end
+  
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+    holiday_hash.each do |season,data|
+    data.each do |key,hols|
+      hols << supply
+    end
+  
+  end
 end
 
-def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+#   # code here
+#   # binding.pry
+#     holiday_hash.each do |season,holiday_name|
+#       holiday_hash[:season].push
+  
+#   # # remember to return the updated hash
+# holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+     binding.pry
+    # holiday_hash.each do |season,hols|
+   
+    #   if season[hols] == :winter
+    #     hols.each do |k,supply|
+    #       supply << k.values
+    #     end
+    #     supply
+    #   end
+    # end
 end
 
 def all_supplies_in_holidays(holiday_hash)
